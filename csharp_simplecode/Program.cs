277ffce599b6119c29e0eb6c9d2aa010011567c0
PJaworski <pjaworski.dev@gmail.com>
2025-08-01 20:54:23 +0200

    Initial commit

---
+++ csharp_simplecode/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dge");
+            Consle.Beep();
+        }
+    }
+}
