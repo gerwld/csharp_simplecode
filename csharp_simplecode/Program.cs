29752d291d549e8948d0f089c07c01be9fac62e9
PJaworski <pjaworski.dev@gmail.com>
2025-08-01 21:16:45 +0200

    l4, top-level statements

--- csharp_simplecode/Program.cs
+++ csharp_simplecode/Program.cs
@@ -1,14 +1,3 @@
-﻿using System; // с обновой sdk,
-              // по идее больше не нужно импортить
-
-namespace Csharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            System.Console.WriteLine("dge");
-            System.Console.Beep();
-        }
-    }
-}
+﻿
+Console.WriteLine(); // top-level statements
+                     // юзлес история
