a94a1ab4cd2385b666e010d811dd10ac4880891d
PJaworski <pjaworski.dev@gmail.com>
2025-08-03 20:40:11 +0200

    l18, &|

---
+++ l18/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace l18;
+
+class Program
+{
+
+    public static bool GetCoolingStatus()
+    {
+        return true;
+    }
+    
+    public static bool GetTemperature()
+    {
+        return true;
+    }
+    
+    static void Main(string[] args)
+    {
+        // && - не проверяет второе условие, поверхностное сравнение
+        // & - побитовое сравнение, всегда проверяет 2 условие
+        // & и |  - для побитовых сравнений (более трудозатратно)
+        
+        
+        if (GetCoolingStatus() || GetTemperature())
+        {
+            Console.WriteLine("Угроза повреждения SOC");    
+        }
+    }
+}
