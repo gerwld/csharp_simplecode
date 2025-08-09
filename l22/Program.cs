0b58ded19462479cc9a4651bc3f03cf41d541d93
PJaworski <pjaworski.dev@gmail.com>
2025-08-09 15:59:07 +0200

    l22 while

---
+++ l22/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace l22;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int counter = 0;
+        int limit = int.Parse(Console.ReadLine());
+        while (counter < limit)
+        {
+            counter++;
+            Console.WriteLine(counter);
+        }
+
+    }
+}
