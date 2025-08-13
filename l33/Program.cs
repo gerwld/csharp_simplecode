03de09fae7356ceb7f2a77acf249ad6f59c6f105
PJaworski <pjaworski.dev@gmail.com>
2025-08-13 11:21:01 +0200

    l33

---
+++ l33/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace l33;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int [] {10,3,5} ;
+
+        
+        for (var i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+        
+        
+        foreach (int i in array)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
